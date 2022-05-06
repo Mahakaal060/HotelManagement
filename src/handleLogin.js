8a5b515b645562dc840476bbfeb149beac0cd457
Gourav Singh Rawat <grvrawat999@gmail.com>
2022-05-06 19:52:01 +0530

    chore: established requests for API

--- src/handleLogin.js
+++ src/handleLogin.js
@@ -1,23 +1,39 @@
 // Variables
-const loginForm = document.querySelector("form");
+const submitLoginBtn = document.getElementById("submitLoginBtn");
 const homeBtn = document.getElementById("homeBtn");
 const signupBtn = document.getElementById("signupBtn");
+const emailInput = document.getElementById("exampleInputEmail1");
+const passwordInput = document.getElementById("exampleInputPassword1");
 
 // Functions
-const handleLogin = () => {
-  console.log("Login here...");
+const handleLogin = async () => {
+  const email = emailInput.value;
+  const password = passwordInput.value;
+  try {
+    const res = await axios.post("http://localhost:5000/", {
+      email: email,
+      password: password,
+      log: "login",
+    });
+    if (res.data) {
+      console.log(res.data);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const goHome = () => {
   location.href = "/";
 };
 
-const goSignUp = () => {
+const goSignUp = (event) => {
+  event.preventDefault();
   location.href = "/signup.html";
 };
 
 // Events
-loginForm.addEventListener("click", function (event) {
+submitLoginBtn.addEventListener("click", function (event) {
   event.preventDefault();
   handleLogin();
 });
